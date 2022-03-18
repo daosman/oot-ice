d4396083b281d4c46608aaf2269a33f1c7fe607a
Dahir Osman <daosman@cisco.com>
2022-03-18 18:22:53 -0400

    Build both STD and RT kernels

--- build.sh
+++ build.sh
@@ -4,30 +4,31 @@ set -eu
 ICE_DRIVER_VER=$1; shift
 OCP_VER=$1; shift
 
+# Point to your local registry
+LOCAL_REGISTRY='cnfde2.ptp.lab.eng.bos.redhat.com:5000'
+
+BASE_IMAGE='registry.access.redhat.com/ubi8-minimal:latest'
+DRIVER_IMAGE='oot-ice'
 PULL_SECRET="${PWD}/pull-secret.txt"
 
 DTK_IMAGE=$(oc adm -a ${PULL_SECRET} release info --image-for=driver-toolkit quay.io/openshift-release-dev/ocp-release:${OCP_VER}-x86_64)
 
-STD_KERNEL_VER=$(podman run --rm ${DTK_IMAGE} rpm -qa | grep 'kernel-core-' | sed 's#kernel-core-##')
-RT_KERNEL_VER=$(podman run --rm ${DTK_IMAGE} rpm -qa | grep 'kernel-rt-core-' | sed 's#kernel-rt-core-##')
+STD_KERNEL_VER=$(podman run --authfile ${PULL_SECRET} --rm ${DTK_IMAGE} rpm -qa | grep 'kernel-core-' | sed 's#kernel-core-##')
+RT_KERNEL_VER=$(podman run --authfile ${PULL_SECRET} --rm ${DTK_IMAGE} rpm -qa | grep 'kernel-rt-core-' | sed 's#kernel-rt-core-##')
 
-KERNEL_VER=${STD_KERNEL_VER}
+echo "DTKI for OCP-${OCP_VER} : ${DTK_IMAGE}"
 
-echo "STD-KERNEL: ${STD_KERNEL_VER}"
-echo "RT-KERNEL : ${RT_KERNEL_VER}"
+for KERNEL_VER in `echo "${STD_KERNEL_VER} ${RT_KERNEL_VER}"`; do
+  echo "Building for ${KERNEL_VER}"
+  TAG=${KERNEL_VER}
 
-BASE_IMAGE='registry.access.redhat.com/ubi8-minimal:latest'
-PULL_SECRET="${PWD}/pull-secret.txt"
-LOCAL_REGISTRY='registry.ran-vcl01.ptp.lab.eng.bos.redhat.com:5000'
-DRIVER_IMAGE='oot-ice'
-TAG=${KERNEL_VER}
+  podman build -f Dockerfile --no-cache . \
+   --build-arg IMAGE=${BASE_IMAGE} \
+   --build-arg BUILD_IMAGE=${DTK_IMAGE} \
+   --build-arg DRIVER_VER=${ICE_DRIVER_VER} \
+   --build-arg KERNEL_VERSION=${KERNEL_VER}\
+   --authfile=${PULL_SECRET} \
+    -t ${LOCAL_REGISTRY}/intel/${DRIVER_IMAGE}:${TAG}
 
-podman build -f Dockerfile --no-cache . \
- --build-arg IMAGE=registry.access.redhat.com/ubi8-minimal:latest \
- --build-arg BUILD_IMAGE=${DTK_IMAGE} \
- --build-arg DRIVER_VER=${ICE_DRIVER_VER} \
- --build-arg KERNEL_VERSION=${KERNEL_VER}\
- --authfile=${PULL_SECRET} \
-  -t ${LOCAL_REGISTRY}/intel/${DRIVER_IMAGE}:${TAG}
-
-podman push --authfile=${PULL_SECRET} --tls-verify=false ${LOCAL_REGISTRY}/intel/${DRIVER_IMAGE}:${TAG}
+  podman push --authfile=${PULL_SECRET} --tls-verify=false ${LOCAL_REGISTRY}/intel/${DRIVER_IMAGE}:${TAG}
+done
